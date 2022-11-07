890818ea9c62329fd854a2eadb8433d27eeab94d
Joaquim Cunha <joaquimafonsocunha@gmail.com>
2022-11-07 18:36:45 +0000

    Mudar UML

--- criar.sql
+++ criar.sql
@@ -10,43 +10,57 @@ DROP TABLE IF EXISTS Golo;
 -- Criar tabelas
 
 CREATE TABLE Equipa (
-    idEquipa NUMERIC(2, 0) PRIMARY KEY,
+    idEquipa NUMERIC(2, 0),
     nome VARCHAR(25) NOT NULL,
-    tipoCondicao VARCHAR(2),
+    PRIMARY KEY(idEquipa),
 );
 
 CREATE TABLE Jornada (
-    num NUMERIC(2, 0) PRIMARY KEY,
+    num NUMERIC(2, 0),
+    PRIMARY KEY(num)
 );
 
 CREATE TABLE Jogador (
-    num NUMERIC(2, 0) PRIMARY KEY,
+    idJogador NUMERIC(3, 0),
+    num NUMERIC(2, 0),
     nome VARCHAR(25) NOT NULL,
     idEquipa NUMERIC(2, 0) REFERENCES Equipa(idEquipa),
+    PRIMARY KEY(idJogador),
 );
 
 CREATE TABLE Classificacao(
-    idEquipa NUMERIC(2, 0) REFERENCES Equipa(idEquipa),
-    numJornada NUMERIC(2, 0) REFERENCES Jornada(num),
+    idEquipa NUMERIC(2, 0),
+    numJornada NUMERIC(2, 0),
     pontos NUMERIC(3, 0), -- Talvez NOT NULL se o zero puder pertencer
     posicao NUMERIC(2, 0) NOT NULL,
     numJogos NUMERIC(2, 0), -- Igual aos pontos
     numVitoria NUMERIC(2, 0), -- ^
     numEmpate NUMERIC(2, 0), -- ^
     numDerrota NUMERIC(2, 0), -- ^
+    tipoCondicao VARCHAR(2),
+    FOREIGN KEY(idEquipa) REFERENCES Equipa(idEquipa),
+    FOREIGN KEY(numJornada) REFERENCES Jornada(num),
 );
 
 CREATE TABLE Jogo (
-    numJornada NUMERIC(2, 0) REFERENCES Jornada(num),
-    equipaVisitante NUMERIC(2, 0) REFERENCES Equipa(idEquipa),
-    equipaVisitada NUMERIC(2, 0) REFERENCES Equipa(idEquipa),
+    numJornada NUMERIC(2, 0),
+    equipaVisitante NUMERIC(2, 0),
+    equipaVisitada NUMERIC(2, 0),
+    FOREIGN KEY(numJornada) REFERENCES Jornada(num),
+    FOREIGN KEY(equipaVisitante) REFERENCES Equipa(idEquipa),
+    FOREIGN KEY(equipaVisitada) REFERENCES Equipa(idEquipa),
 );
 
 CREATE TABLE Golo (
-    minuto NUMERIC(3, 0) PRIMARY KEY,
-    equipaMarcado NUMERIC(2, 0) REFERENCES Equipa(idEquipa),
-    equipaSofrido NUMERIC(2, 0) REFERENCES Equipa(idEquipa),
-    numJornada NUMERIC(2, 0) REFERENCES Jornada(num),
-    numJogador NUMERIC(2, 0) REFERENCES Jogador(num),
+    minuto NUMERIC(3, 0),
+    equipaMarcado NUMERIC(2, 0),
+    equipaSofrido NUMERIC(2, 0),
+    numJornada NUMERIC(2, 0),
+    idJogador NUMERIC(2, 0),
+    PRIMARY KEY(minuto),
+    FOREIGN KEY(equipaMarcado) REFERENCES Equipa(idEquipa),
+    FOREIGN KEY(equipaSofrido) REFERENCES Equipa(idEquipa),
+    FOREIGN KEY(numJornada) REFERENCES Jornada(num),
+    FOREIGN KEY(idJogador) REFERENCES Jogador(idJogador),
 );
 
