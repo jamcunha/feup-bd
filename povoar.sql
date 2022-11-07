9b1648ba5de850e22e1757d3001cebe9f2bedd9b
Afonso Neves <up202108884@up.pt>
2022-11-07 17:56:25 +0000

    povoar templates

---
+++ povoar.sql
@@ -0,0 +1,13 @@
+-- Popular tabelas
+
+INSERT INTO Equipa (idEquipa, nome, tipoCondicao)  VALUES ();
+
+INSERT INTO Jornada (num) VALUES ();
+
+INSERT INTO Jogador (num, nome, idEquipa) VALUES ();
+
+INSERT INTO Classificacao( idEquipa, numJornada, pontos, posicao, numJogos, numVitoria, numEmpate, numDerrota) VALUES ();
+
+INSERT INTO Jogo (numJornada, equipaVisitante, equipaVisitada) VALUES ();
+
+INSERT INTO Golo (minuto, equipaMarcado, equipaSofrido, numJornada, numJogador) VALUES ();
