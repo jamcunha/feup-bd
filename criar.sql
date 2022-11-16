07526b838a4852c688fdead571861ed516491071
Joaquim Cunha <joaquimafonsocunha@gmail.com>
2022-11-16 22:07:42 +0000

    fixed some errors

--- criar.sql
+++ criar.sql
@@ -10,45 +10,45 @@ DROP TABLE IF EXISTS Golo;
 -- Criar tabelas
 
 CREATE TABLE Equipa (
-    idEquipa INT PRIMARY KEY AUTOINCREMENT,
-    nome VARCHAR(50) NOT NULL UNIQUE,
+    idEquipa INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome VARCHAR(50) NOT NULL UNIQUE
 );
 
 CREATE TABLE Jornada (
-    num INT PRIMARY KEY CHECK(num > 0 AND num <= 34),
+    num INTEGER PRIMARY KEY CHECK(num > 0 AND num <= 34)
 );
 
 CREATE TABLE Jogador (
-    idJogador INT PRIMARY KEY AUTOINCREMENT,
-    num INT NOT NULL CHECK(num > 0 AND num < 100),
+    idJogador INTEGER PRIMARY KEY AUTOINCREMENT,
+    num INTEGER NOT NULL CHECK(num > 0 AND num < 100),
     nome VARCHAR(50) NOT NULL,
-    idEquipa INT REFERENCES Equipa(idEquipa),
+    idEquipa INTEGER REFERENCES Equipa(idEquipa)
 );
 
 CREATE TABLE Classificacao(
-    idEquipa INT REFERENCES Equipa(idEquipa),
-    numJornada INT REFERENCES Jornada(num) CHECK(numJornada > 0 AND numJornada <= 34),
-    pontos INT NOT NULL CHECK(pontos >= 0),
-    posicao INT NOT NULL CHECK(posicao >= 1 AND posicao <= 18),
-    numJogos INT NOT NULL CHECK(numJogos >= 1 AND numJogos <= 34 AND numJogos = numVitoria + numEmpate p numDerrota),
-    numVitoria INT NOT NULL CHECK(numVitoria >= 0 AND numVitoria <= numJogos - numEmpate - numDerrota),
-    numEmpate INT NOT NULL CHECK(numEmpate >= 0 AND numEmpate <= numJogos - numVitoria - numDerrota),
-    numDerrota INT NOT NULL CHECK(numDerrota >= 0 AND numDerrota <= numJogos - numVitoria - numEmpate),
-    tipoCondicao VARCHAR(2), -- Condicoes depois nos triggers
+    idEquipa INTEGER REFERENCES Equipa(idEquipa),
+    numJornada INTEGER REFERENCES Jornada(num) CHECK(numJornada > 0 AND numJornada <= 34),
+    pontos INTEGER NOT NULL CHECK(pontos >= 0),
+    posicao INTEGER NOT NULL CHECK(posicao >= 1 AND posicao <= 18),
+    numJogos INTEGER NOT NULL CHECK(numJogos >= 1 AND numJogos <= 34 AND numJogos = numVitoria + numEmpate + numDerrota),
+    numVitoria INTEGER NOT NULL CHECK(numVitoria >= 0 AND numVitoria <= numJogos - numEmpate - numDerrota),
+    numEmpate INTEGER NOT NULL CHECK(numEmpate >= 0 AND numEmpate <= numJogos - numVitoria - numDerrota),
+    numDerrota INTEGER NOT NULL CHECK(numDerrota >= 0 AND numDerrota <= numJogos - numVitoria - numEmpate),
+    tipoCondicao VARCHAR(2) -- Condicoes depois nos triggers
 );
 
 CREATE TABLE Jogo (
-    idJogo INT PRIMARY KEY AUTOINCREMENT,
-    vencedor INT REFERENCES Equipa(idEquipa) CHECK(vencedor = equipaVisitada OR vencedor = equipaVisitante),
-    numJornada INT REFERENCES Jornada(num) CHECK(numJornada > 0 AND numJornada <= 34),
-    equipaVisitada INT REFERENCES Equipa(idEquipa),
-    equipaVisitante INT REFERENCES Equipa(idEquipa),
+    idJogo INTEGER PRIMARY KEY AUTOINCREMENT,
+    vencedor INTEGER REFERENCES Equipa(idEquipa) CHECK(vencedor = equipaVisitada OR vencedor = equipaVisitante),
+    numJornada INTEGER REFERENCES Jornada(num) CHECK(numJornada > 0 AND numJornada <= 34),
+    equipaVisitada INTEGER REFERENCES Equipa(idEquipa),
+    equipaVisitante INTEGER REFERENCES Equipa(idEquipa)
 );
 
 CREATE TABLE Golo (
-    minuto INT PRIMARY KEY CHECK(minuto > 0),
-    idJogo INT REFERENCES Jogo(idJogo),
-    equipaMarc INT REFERENCES Equipa(idEquipa),
-    idJogador INT REFERENCES Jogador(idJogador),
+    minuto INTEGER PRIMARY KEY CHECK(minuto > 0),
+    idJogo INTEGER REFERENCES Jogo(idJogo),
+    equipaMarc INTEGER REFERENCES Equipa(idEquipa),
+    idJogador INTEGER REFERENCES Jogador(idJogador)
 );
 
