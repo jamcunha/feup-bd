7938a29101f6460cb80dd3949609b5c700db354c
joaoprcoutinho <up202108787@up.pt>
2022-12-08 19:20:35 +0000

    empty message

--- Queries/int1.sql
+++ Queries/int1.sql
@@ -3,7 +3,8 @@
 .nullvalue NULL
 
 SELECT Equipa.nome, Classificacao.tipoCondicao AS Condicao 
-FROM Equipa JOIN Classificacao ON idEquipa AND ((Classificacao.posicao>=1 AND Classificacao.posicao<=5) OR (Classificacao.posicao>=16 AND Classificacao.posicao<=18);
+FROM Equipa JOIN Classificacao ON idEquipa
+WHERE (Classificacao.posicao>=1 AND Classificacao.posicao<=5) OR (Classificacao.posicao>=16 AND Classificacao.posicao<=18);
 
 
 
