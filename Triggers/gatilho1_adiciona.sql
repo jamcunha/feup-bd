163b5796b02447e3599e1240c2d7b81705c19a90
joaoprcoutinho <up202108787@up.pt>
2022-12-21 23:27:29 +0000

    entregar este para já

--- Triggers/gatilho1_adiciona.sql
+++ Triggers/gatilho1_adiciona.sql
@@ -7,15 +7,57 @@ PRAGMA foreign_keys = ON;
 CREATE TRIGGER IF NOT EXISTS insertClassificacao
 AFTER INSERT ON Jogo
 FOR EACH ROW
-BEGIN	
+BEGIN
+
+		--INSERT INTO Classificacao (idEquipa, numJornada, pontos, numVitoria, numEmpate, numDerrota, diferencaGolos)
+		--	SELECT 	N.equipaVisitante,
+		--		N.numJornada,
+		--		ifnull(C.pontos,0)+CASE WHEN new.equipaVisitante = new.Vencedor 
+		--					     THEN 3 
+		--					WHEN new.Vencedor IS NUll
+		--					     THEN 1 
+		--					ELSE 0
+		--				   END,
+		--		ifnull(C.pontos,0)+CASE WHEN new.equipaVisitante = new.Vencedor 
+		--					     THEN 3 
+		--					WHEN new.Vencedor IS NUll
+		--					     THEN 1 
+		--					ELSE 0
+		--				   END,	
+		--		diferencaGolos + golosmarcados do new - golos sofridos do new diferencaGolos	   
+		--				   
+		--	  FROM new N
+		--	  LEFT JOIN Classificacao C ON N.equipaVisitante = C.idEquipa
+		--	  			   AND N.numJornada-1 = C.numJornada
+		--        LEFT JOIN (SELECT  G.equipaMarc,
+		--			     G.idJogo,
+		--			     count(*)
+		--			FROM Golo G
+		--			GROUP BY G.equipaMarc,
+		--				 G.idJogo
+		--		    ) G ON N.idJogo = G.idJogo,
+		--		    	   N.equipaVisitante = G.equipaMarc
+				    	   
+					
 			  			   	
+		
 		
 
 		INSERT INTO Classificacao (idEquipa, numJornada, pontos, numVitoria, numEmpate, numDerrota, diferencaGolos)
-		VALUES (new.equipaVisitante, new.numJornada,0,0,1,0,0);
+		VALUES (new.equipaVisitante, new.numJornada,
+		ifnull((SELECT pontos FROM Classificacao WHERE numJornada=new.numJornada-1 AND Classificacao.idEquipa=new.equipaVisitante),0) + (CASE WHEN new.equipaVisitante = new.Vencedor THEN 3 WHEN new.Vencedor IS NUll THEN 1 ELSE 0 END),
+		ifnull((SELECT numVitoria FROM Classificacao WHERE numJornada=new.numJornada-1 AND Classificacao.idEquipa=new.equipaVisitante),0) + (CASE WHEN new.equipaVisitante = new.Vencedor THEN 1 ELSE 0 END),
+		ifnull((SELECT numEmpate FROM Classificacao WHERE numJornada=new.numJornada-1 AND Classificacao.idEquipa=new.equipaVisitante),0) + (CASE WHEN new.Vencedor IS Null THEN 1 ELSE 0 END), 
+		ifnull((SELECT numDerrota FROM Classificacao WHERE numJornada=new.numJornada-1 AND Classificacao.idEquipa=new.equipaVisitante),0) + (CASE WHEN new.equipaVisitada = new.Vencedor  THEN 1 ELSE 0 END),
+		ifnull((SELECT diferencaGolos FROM Classificacao WHERE numJornada=new.numJornada-1 AND Classificacao.idEquipa=new.equipaVisitante),0) - (SELECT count(*) as Golos FROM Jogo,Golo WHERE Jogo.idJogo=Golo.idJogo and Jogo.equipaVisitada = Golo.equipaMarc and Golo.idJogo=new.idJogo) + (SELECT count(*) as Golo FROM Jogo,Golo WHERE Jogo.idJogo=Golo.idJogo and Jogo.equipaVisitante= Golo.equipaMarc and Golo.idJogo=new.idJogo));
 
 		INSERT INTO Classificacao (idEquipa, numJornada, pontos, numVitoria, numEmpate, numDerrota, diferencaGolos)
-		VALUES (new.equipaVisitada, new.numJornada,0,0,1,0,0);
+		VALUES (new.equipaVisitada, new.numJornada,
+		ifnull((SELECT pontos FROM Classificacao WHERE numJornada=new.numJornada-1 AND Classificacao.idEquipa=new.equipaVisitada),0) + (CASE WHEN new.equipaVisitada = new.Vencedor THEN 3 WHEN new.Vencedor IS NUll THEN 1 ELSE 0 END),
+		ifnull((SELECT numVitoria FROM Classificacao WHERE numJornada=new.numJornada-1 AND Classificacao.idEquipa=new.equipaVisitada),0) + (CASE WHEN new.equipaVisitada = new.Vencedor THEN 1 ELSE 0 END),
+		ifnull((SELECT numEmpate FROM Classificacao WHERE numJornada=new.numJornada-1 AND Classificacao.idEquipa=new.equipaVisitada),0) + (CASE WHEN new.Vencedor IS Null THEN 1 ELSE 0 END), 
+		ifnull((SELECT numDerrota FROM Classificacao WHERE numJornada=new.numJornada-1 AND Classificacao.idEquipa=new.equipaVisitada),0) + (CASE WHEN new.equipaVisitante = new.Vencedor  THEN 1 ELSE 0 END),
+		ifnull((SELECT diferencaGolos FROM Classificacao WHERE numJornada=new.numJornada-1 AND Classificacao.idEquipa=new.equipaVisitada),0) - (SELECT count(*) as Golos FROM Jogo,Golo WHERE Jogo.idJogo=Golo.idJogo and Jogo.equipaVisitante = Golo.equipaMarc and Golo.idJogo=new.idJogo) + (SELECT count(*) as Golo FROM Jogo,Golo WHERE Jogo.idJogo=Golo.idJogo and Jogo.equipaVisitada= Golo.equipaMarc and Golo.idJogo=new.idJogo));
 		
 		
 
