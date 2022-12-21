826ca57d63705da5190f8f34b8e5020a264759bb
joaoprcoutinho <up202108787@up.pt>
2022-12-21 16:56:49 +0000

    trigger classificacao working

--- Queries/int1.sql
+++ Queries/int1.sql
@@ -24,7 +24,7 @@ SELECT 	C.Jornada,
   FROM(
 	SELECT 	C.numJornada 								 	  Jornada, 
 		E.nome 										  Equipa, 
-		row_number() over (partition by C.numJornada order by C.pontos, C.diferencaGolos) Posicao
+		row_number() over (partition by C.numJornada order by C.pontos DESC, C.diferencaGolos DESC) Posicao
 	  FROM Equipa E
 	  JOIN Classificacao C ON C.idEquipa=E.idEquipa
       ) C
--- Triggers/gatilho1_adiciona.sql
+++ Triggers/gatilho1_adiciona.sql
@@ -64,31 +64,36 @@ BEGIN
 	
 END;
 
-
 CREATE TRIGGER IF NOT EXISTS updateClassificacao
 AFTER UPDATE ON Jogo
 FOR EACH ROW
 BEGIN
 	UPDATE Classificacao
-	SET pontos = ifnull((SELECT pontos FROM Classificacao WHERE numJornada=new.numJornada-1 AND Classificacao.idEquipa=new.equipaVisitada),0) + (CASE WHEN new.equipaVisitada = new.Vencedor THEN 3 WHEN new.Vencedor IS NUll THEN 1 ELSE 0 END),
-	numVitoria = ifnull((SELECT numVitoria FROM Classificacao WHERE numJornada=new.numJornada-1 AND Classificacao.idEquipa=new.equipaVisitada),0) + (CASE WHEN new.equipaVisitada = new.Vencedor THEN 1 ELSE 0 END),
-	numEmpate = ifnull((SELECT numEmpate FROM Classificacao WHERE numJornada=new.numJornada-1 AND Classificacao.idEquipa=new.equipaVisitada),0) + (CASE WHEN new.Vencedor IS Null THEN 1 ELSE 0 END), 
-	numDerrota = ifnull((SELECT numDerrota FROM Classificacao WHERE numJornada=new.numJornada-1 AND Classificacao.idEquipa=new.equipaVisitada),0) + (CASE WHEN new.equipaVisitante = new.Vencedor  THEN 1 ELSE 0 END),
-	diferencaGolos = ifnull((SELECT diferencaGolos FROM Classificacao WHERE numJornada=new.numJornada-1 AND Classificacao.idEquipa=new.equipaVisitada),0) - (SELECT count(*) as Golos FROM Jogo,Golo WHERE Jogo.idJogo=Golo.idJogo and Jogo.equipaVisitante = Golo.equipaMarc and Golo.idJogo=new.idJogo) + (SELECT count(*) as Golo FROM Jogo,Golo WHERE Jogo.idJogo=Golo.idJogo and Jogo.equipaVisitada= Golo.equipaMarc and Golo.idJogo=new.idJogo)
+	SET pontos = pontos + (CASE WHEN new.equipaVisitada = new.Vencedor THEN 3 WHEN new.Vencedor IS NUll THEN 1 ELSE 0 END) - (CASE WHEN new.equipaVisitada = old.Vencedor THEN 3 WHEN old.Vencedor IS NUll THEN 1 ELSE 0 END),
+	numVitoria = numVitoria + (CASE WHEN new.equipaVisitada = new.Vencedor THEN 1 ELSE 0 END) - (CASE WHEN new.equipaVisitada = old.Vencedor THEN 1 ELSE 0 END),
+	numEmpate = numEmpate + (CASE WHEN new.Vencedor IS Null THEN 1 ELSE 0 END) - (CASE WHEN old.Vencedor IS Null THEN 1 ELSE 0 END), 
+	numDerrota = numDerrota + (CASE WHEN new.equipaVisitante = new.Vencedor  THEN 1 ELSE 0 END) - (CASE WHEN new.equipaVisitante = old.Vencedor  THEN 1 ELSE 0 END),
+	diferencaGolos = -(SELECT count(*) as Golos FROM Golo JOIN Jogo ON Golo.idJogo=Jogo.idJogo WHERE Jogo.numJornada<=new.numJornada AND Golo.equipaMarc!=new.equipaVisitada AND (Jogo.equipaVisitante=new.equipaVisitada OR Jogo.equipaVisitada=new.equipaVisitada)) + (SELECT count(*) as Golos FROM Golo JOIN Jogo ON Golo.idJogo=Jogo.idJogo WHERE Jogo.numJornada<=new.numJornada AND Golo.equipaMarc=new.equipaVisitada )
 	WHERE new.equipaVisitada = Classificacao.idEquipa AND Classificacao.numJornada>=new.numJornada;
 	
-		
+	
 	UPDATE Classificacao
-	SET pontos = ifnull((SELECT pontos FROM Classificacao WHERE numJornada=new.numJornada-1 AND Classificacao.idEquipa=new.equipaVisitante),0) + (CASE WHEN new.equipaVisitante = new.Vencedor THEN 3 WHEN new.Vencedor IS NUll THEN 1 ELSE 0 END),
-	numVitoria = ifnull((SELECT numVitoria FROM Classificacao WHERE numJornada=new.numJornada-1 AND Classificacao.idEquipa=new.equipaVisitante),0) + (CASE WHEN new.equipaVisitante = new.Vencedor THEN 1 ELSE 0 END),
-	numEmpate = ifnull((SELECT numEmpate FROM Classificacao WHERE numJornada=new.numJornada-1 AND Classificacao.idEquipa=new.equipaVisitante),0) + (CASE WHEN new.Vencedor IS Null THEN 1 ELSE 0 END), 
-	numDerrota = ifnull((SELECT numDerrota FROM Classificacao WHERE numJornada=new.numJornada-1 AND Classificacao.idEquipa=new.equipaVisitante),0) + (CASE WHEN new.equipaVisitada = new.Vencedor  THEN 1 ELSE 0 END),
-	diferencaGolos = ifnull((SELECT diferencaGolos FROM Classificacao WHERE numJornada=new.numJornada-1 AND Classificacao.idEquipa=new.equipaVisitante),0) - (SELECT count(*) as Golos FROM Jogo,Golo WHERE Jogo.idJogo=Golo.idJogo and Jogo.equipaVisitada = Golo.equipaMarc and Golo.idJogo=new.idJogo) + (SELECT count(*) as Golo FROM Jogo,Golo WHERE Jogo.idJogo=Golo.idJogo and Jogo.equipaVisitante= Golo.equipaMarc and Golo.idJogo=new.idJogo)
-	WHERE (new.equipaVisitante = Classificacao.idEquipa AND Classificacao.numJornada<=new.numJornada);
+	SET pontos = pontos + (CASE WHEN new.equipaVisitante = new.Vencedor THEN 3 WHEN new.Vencedor IS NUll THEN 1 ELSE 0 END) - (CASE WHEN new.equipaVisitante = old.Vencedor THEN 3 WHEN old.Vencedor IS NUll THEN 1 ELSE 0 END),
+	numVitoria = numVitoria + (CASE WHEN new.equipaVisitante = new.Vencedor THEN 1 ELSE 0 END) - (CASE WHEN new.equipaVisitante = old.Vencedor THEN 1 ELSE 0 END),
+	numEmpate = numEmpate + (CASE WHEN new.Vencedor IS Null THEN 1 ELSE 0 END) - (CASE WHEN old.Vencedor IS Null THEN 1 ELSE 0 END), 
+	numDerrota = numDerrota + (CASE WHEN new.equipaVisitada = new.Vencedor  THEN 1 ELSE 0 END) - (CASE WHEN new.equipaVisitada = old.Vencedor  THEN 1 ELSE 0 END),
+	diferencaGolos = -(SELECT count(*) as Golos FROM Golo JOIN Jogo ON Golo.idJogo=Jogo.idJogo WHERE Jogo.numJornada<=new.numJornada AND Golo.equipaMarc!=new.equipaVisitante AND (Jogo.equipaVisitante=new.equipaVisitante OR Jogo.equipaVisitada=new.equipaVisitante)) + (SELECT count(*) as Golos FROM Golo JOIN Jogo ON Golo.idJogo=Jogo.idJogo WHERE Jogo.numJornada<=new.numJornada AND Golo.equipaMarc=new.equipaVisitante )
+	WHERE new.equipaVisitante = Classificacao.idEquipa AND Classificacao.numJornada>=new.numJornada;
+	
+		
+	
 	
 
 END;
 	
+
+
+
 	
 
 --REMOVER GOLO?
